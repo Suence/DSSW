dd14d52f0d83bf9c40f928c695b0869c07dbd0e8
SunChaoJie <ToBinary@outlook.com>
2020-04-30 13:49:27 +0800

    监听目标文件夹的新图片并显示在 UI 上; 复制新图片到剪贴板功能;

---
+++ SRC/DSSW.Client/Converters/PathToBitmapImageConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace DSSW.Client.Converters
+{
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    public class PathToBitmapImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.UriSource = new Uri($"{value}");
+                bitmapImage.EndInit();
+                return bitmapImage;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
