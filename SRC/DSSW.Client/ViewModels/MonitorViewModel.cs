ed6aadee4408b8bf88ff9b0a2fc29341a39d34a4
SunChaoJie <ToBinary@outlook.com>
2020-04-30 13:49:27 +0800

    监听目标文件夹并显示在 UI 上

--- SRC/DSSW.Client/ViewModels/MonitorViewModel.cs
+++ SRC/DSSW.Client/ViewModels/MonitorViewModel.cs
@@ -1,12 +1,47 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace DSSW.Client.ViewModels
 {
     public class MonitorViewModel : BindableBase
     {
+        #region private
+        private string _newScreenShotPath;
+        private FileSystemWatcher _fileSystemWatcher;
+        #endregion
+
+        public string NewScreenShotPath
+        {
+            get => _newScreenShotPath;
+            set => SetProperty(ref _newScreenShotPath, value);
+        }
+
+        public DelegateCommand CopyToClipboardCommand { get; }
+        private void CopyToClipboard()
+            => Clipboard.SetImage(new BitmapImage(new Uri(NewScreenShotPath)));
+
         public MonitorViewModel()
         {
+            CopyToClipboardCommand = new DelegateCommand(CopyToClipboard);
+            MonitorTargetFolder(@"D:\TEST");
+        }
 
+        private void MonitorTargetFolder(string targetFolderFullPath)
+        {
+            _fileSystemWatcher = new FileSystemWatcher(targetFolderFullPath)
+            {
+                IncludeSubdirectories = true
+            };
+            _fileSystemWatcher.Created += NewScreenShotCreated;
+            _fileSystemWatcher.EnableRaisingEvents = true;
+        }
+        private void NewScreenShotCreated(object sender, FileSystemEventArgs e)
+        {
+            NewScreenShotPath = e.FullPath;
         }
     }
 }
