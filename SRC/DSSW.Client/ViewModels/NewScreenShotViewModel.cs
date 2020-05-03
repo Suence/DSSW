f6ce460a579c6b49deeef18dcc3ac8fb549e5318
ChaoJie Sun <Suence>
2020-05-03 20:19:02 +0800

    新截图窗口一段时间无操作自动关闭

--- SRC/DSSW.Client/ViewModels/NewScreenShotViewModel.cs
+++ SRC/DSSW.Client/ViewModels/NewScreenShotViewModel.cs
@@ -15,7 +15,16 @@ namespace DSSW.Client.ViewModels
         private string _newScreenShotPath;
         private readonly IRegionManager _regionManager;
         private bool _isCopied;
+        private bool _canAutoExit;
+        private bool _isFocused;
+
         #endregion
+
+        public bool CanAutoExit
+        {
+            get => _canAutoExit;
+            set => SetProperty(ref _canAutoExit, value);
+        }
 
         public bool IsCopied
         {
@@ -40,16 +49,21 @@ namespace DSSW.Client.ViewModels
 
 
         private void BackToMonitor()
-            => _regionManager.RequestNavigate(
-                RegionNames.MainRegion,
-                ViewNames.Monitor);
+        {
+            _regionManager.RequestNavigate(
+                           RegionNames.MainRegion,
+                           ViewNames.Monitor);
+            _isFocused = true;
+        }
 
         public NewScreenShotViewModel(IRegionManager regionManager)
         {
             _regionManager = regionManager;
             CopyToClipboardCommand = new DelegateCommand(CopyToClipboard);
             BackToMonitorCommand = new DelegateCommand(BackToMonitor);
-            //AutoBackToMonitor();
+            StartTimeCommand = new DelegateCommand(Start);
+            ResetTimeCommand = new DelegateCommand(ResetTime);
+            Start();
         }
 
         public async void AutoBackToMonitor()
@@ -57,13 +71,32 @@ namespace DSSW.Client.ViewModels
             await Task.Delay(TimeSpan.FromSeconds(10));
             BackToMonitor();
         }
-
+        public DelegateCommand StartTimeCommand { get; }
+        private async void Start()
+        {
+            _isFocused = false;
+            for (int i = 0; i < 100; ++i)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(0.1));
+                System.Diagnostics.Debug.WriteLine("Running");
+                if (_isFocused)
+                {
+                    return;
+                }
+            }
+            CanAutoExit = true;
+        }
+        public DelegateCommand ResetTimeCommand { get; }
+        private void ResetTime()
+        {
+            _isFocused = true;
+        }
         public bool IsNavigationTarget(NavigationContext navigationContext)
             => true;
 
         public void OnNavigatedFrom(NavigationContext navigationContext)
         {
-           
+
         }
 
         public void OnNavigatedTo(NavigationContext navigationContext)
