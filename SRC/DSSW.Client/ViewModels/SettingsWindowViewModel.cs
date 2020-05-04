1a04c6ae4090783558fe449d06ffb3807b5d5e60
ChaoJie Sun <Suence>
2020-05-04 20:39:49 +0800

    更新 COLG 链接

--- SRC/DSSW.Client/ViewModels/SettingsWindowViewModel.cs
+++ SRC/DSSW.Client/ViewModels/SettingsWindowViewModel.cs
@@ -10,7 +10,7 @@ namespace DSSW.Client.ViewModels
     public class SettingsWindowViewModel : BindableBase
     {
         public string Title { get; set; } = "程序设置";
-        public string ColgAddress { get; set; } = "https://bbs.colg.cn";
+        public string ColgAddress { get; set; } = "https://bbs.colg.cn/thread-7855396-1-1.html";
         public string TiebaAddress { get; set; } = "https://tieba.baidu.com";
 
         public DelegateCommand<string> GoToBroswerCommand { get; set; }
